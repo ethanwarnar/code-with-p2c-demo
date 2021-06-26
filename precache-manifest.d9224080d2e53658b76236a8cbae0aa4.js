739c3fffbfc4242dcd1d1020b4d51eab0135af6c
Ethan Warnar <enpwarna@uwaterloo.ca>
2021-06-26 07:01:49 -0400

    Updates

---
+++ precache-manifest.d9224080d2e53658b76236a8cbae0aa4.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ade08e5ce4638942ec64c0a2b3c81f6",
+    "url": "/code-with-p2c/index.html"
+  },
+  {
+    "revision": "7f02748715b5bb280d14",
+    "url": "/code-with-p2c/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "d81f6df6a8a52aa44ca9",
+    "url": "/code-with-p2c/static/css/main.47cf6363.chunk.css"
+  },
+  {
+    "revision": "7f02748715b5bb280d14",
+    "url": "/code-with-p2c/static/js/2.5c726ae7.chunk.js"
+  },
+  {
+    "revision": "11a928b57aca5049fe950bf8bcde77c5",
+    "url": "/code-with-p2c/static/js/2.5c726ae7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d81f6df6a8a52aa44ca9",
+    "url": "/code-with-p2c/static/js/main.d681eff5.chunk.js"
+  },
+  {
+    "revision": "b51424c070c504abf31a",
+    "url": "/code-with-p2c/static/js/runtime-main.93dcca58.js"
+  },
+  {
+    "revision": "945eefa280970fcf7fb1eddb1a9321b9",
+    "url": "/code-with-p2c/static/media/P2C_coding_pic_IG.945eefa2.PNG"
+  },
+  {
+    "revision": "5628a15b10e710b2c4d952ec9a5902cd",
+    "url": "/code-with-p2c/static/media/jakethedog.5628a15b.jpeg"
+  },
+  {
+    "revision": "7767753ccf36009edeea2b8938dd60b0",
+    "url": "/code-with-p2c/static/media/p2c.7767753c.png"
+  }
+]);
